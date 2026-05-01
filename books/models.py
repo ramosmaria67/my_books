423e6b539e218fe70265cb853a6ae1b8c0fbdecc
chrizmeza <angelariasmezacrack@gmail.com>
2026-05-01 12:30:05 -0700

    agrege cosas en admin y en models, agrege tambien en apps mi app books

--- books/models.py
+++ books/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+# New seccion of the code added (To get All URL Generated).
+class Category(models.Model):
+    title = models.CharField(max_length=250)
+   
+class Book(models.Model):
+
+    category = models.ForeignKey(Category, related_name='Book', on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)  
+    author = models.CharField(max_length=250)  
+    image = models.CharField(max_length=2000)
+
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
