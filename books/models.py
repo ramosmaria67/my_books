adc927fa33fd1cfe86359a9df38bd5aac8bb4dd2
chrizmeza <angelariasmezacrack@gmail.com>
2026-05-01 12:40:54 -0700

    añadi urls en mi app y agrege cosas en los dos urls

--- books/models.py
+++ books/models.py
@@ -6,11 +6,17 @@ class Category(models.Model):
    
 class Book(models.Model):
 
+    ACTIVE = 'active'
+    DRAFT = 'draft'
+
+    CHOICES_STATUS = {
+        (ACTIVE, 'Active'),
+        (DRAFT, 'Draft')
+    }
+
     category = models.ForeignKey(Category, related_name='Book', on_delete=models.CASCADE)
     title = models.CharField(max_length=250)  
     author = models.CharField(max_length=250)  
     image = models.CharField(max_length=2000)
-
-
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- books/views.py
+++ books/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+    # -*- coding: utf-8 -*-
+from django.db.models import Q
+from django.shortcuts import render, get_object_or_404
+
+from .models import Book, Category
 
 # Create your views here.
+def home(request):
+    books = Book.objects.filter(status=Book.ACTIVE).order_by('-created_at')
+    context = {
+        'books': books
+    }
+    return render(request, 'book/home.html', context)
+
+def detail(request, id):
+    books = get_object_or_404(Book, id=id, status=Book.ACTIVE)
+    context = {
+        'books': books,
+    }
+    return render(request, 'book/detail_book.html', context)
